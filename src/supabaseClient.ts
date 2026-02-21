2dcbc6371acb9c10c7d0ec55d131d478c31bdbcf
pgonia8-wq <pgonia8@gmail.com>
2026-02-21 02:40:16 -0600

    Create supabaseClient.ts

---
+++ src/supabaseClient.ts
@@ -0,0 +1,6 @@
+import { createClient } from '@supabase/supabase-js'
+
+export const supabase = createClient(
+  'https://vtjqfzpfehfofamhowjz.supabase.co', 
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ0anFmenBmZWhmb2ZhbWhvd2p6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE2NDMxMTgsImV4cCI6MjA4NzIxOTExOH0.XShaoDlgVKT2LHk0fYOT5TWGgwkfn3bQQbuV2pcw_HM'
+)
