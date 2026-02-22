04ac7cf2f18c485e60ea9810cde685e146e0afef
pgonia8-wq <pgonia8@gmail.com>
2026-02-21 18:33:26 -0600

    vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,12 @@
+// vite.config.ts
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist', // <-- aquí le dices que genere 'dist'
+    outDir: 'dist',
+    sourcemap: true,
   },
+  base: './', // <-- esto hace que los assets se busquen en la carpeta correcta
 })
