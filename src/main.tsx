c10b5e81ea55046ace3ba933076a7577418eb44c
pgonia8-wq <pgonia8@gmail.com>
2026-02-22 22:14:20 -0600

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
-import './index.css';  // si tienes estilos globales
-import { MiniKitProvider } from '@worldcoin/minikit-js/minikit-provider';
+import App from './App';
+import './index.css';
+import { MiniAppProvider } from '@worldcoin/minikit-js';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <MiniKitProvider>
+    <MiniAppProvider appId="app_18e24371c2f0aeaa6348745bf40add01">
       <App />
-    </MiniKitProvider>
+    </MiniAppProvider>
   </React.StrictMode>,
 );
