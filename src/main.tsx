00a39d99bb3762caa71c0cbe2bda42c042cfa592
pgonia8-wq <pgonia8@gmail.com>
2026-02-24 17:54:26 -0600

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,20 @@
-import React from 'react';
+Main.tsx Replit 
+import React, { useEffect } from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 import { MiniKit } from '@worldcoin/minikit-js';
 
-// Instala MiniKit UNA SOLA VEZ, lo más temprano posible (fuera de useEffect y StrictMode)
-MiniKit.install('app_6a98c88249208506dcd4e04b529111fc');
+const Root = () => {
+  useEffect(() => {
+    MiniKit.install();
+  }, []);
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  // Quitamos StrictMode temporalmente para evitar double install
-  // Puedes probar agregarlo de nuevo después si todo funciona
-  <App />
-);
+  return (
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+};
+
+ReactDOM.createRoot(document.getElementById('root')!).render(<Root />);
