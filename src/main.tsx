1aabdd94ccb9fa7e3d4c037fd95a66afa3a7d5f1
pgonia8-wq <pgonia8@gmail.com>
2026-03-06 15:17:01 -0600

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -10,17 +10,15 @@ import HomePage from './pages/HomePage.tsx'
 import ChatPage from './pages/chat/ChatPage.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <MiniKitProvider appId="app_6a98c88249208506dcd4e04b529111fc">
-      <ThemeProvider>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/chat" element={<ChatPage />} />
-            <Route path="*" element={<HomePage />} />
-          </Routes>
-        </BrowserRouter>
-      </ThemeProvider>
-    </MiniKitProvider>
-  </React.StrictMode>
+  <MiniKitProvider appId="app_6a98c88249208506dcd4e04b529111fc">
+    <ThemeProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/chat" element={<ChatPage />} />
+          <Route path="*" element={<HomePage />} />
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
+  </MiniKitProvider>
 )
